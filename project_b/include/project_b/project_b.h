f828390e48e9c4e33d021c9824b7cfbb1e3a70ea
Hannes Vogt <hannes@havogt.de>
2018-07-31 16:20:19 +0200

    add a header only library

--- project_b/include/project_b/project_b.h
+++ project_b/include/project_b/project_b.h
@@ -1,3 +1,12 @@
 #pragma once
 
+#include <project_a_header_only/project_a_header_only.h>
+#include <iostream>
+
 void project_b_function();
+
+template <typename T>
+void project_b_header_only_function(T t) {
+  std::cout << "project_b_header_only_function() -> forwarding t" << std::endl;
+  project_a_header_only_function(t);
+}
--- project_c/src/main.cpp
+++ project_c/src/main.cpp
@@ -1,5 +1,5 @@
+// WILL NOT WORK ON PURPOSE: we included project_a as private
 //#include <project_a/public/project_a_public.h>
-// project a not exported as public (from project_b)
 
 #include <project_b/project_b.h>
 #include <iostream>
@@ -8,7 +8,8 @@ int main() {
   std::cout << "from project c call project_b_function():\n ";
   project_b_function();
 
-  // WILL NOT WORK ON PURPOSE: In project_b I added project_a as private.
-  //  std::cout << "trying to call project_a directly" << std::endl;
+  project_b_header_only_function(15);
+
+  // WILL NOT WORK ON PURPOSE: see above
   //  project_a_my_public_function();
 }
